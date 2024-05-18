f631c44780c6ebd27f97b22975183d33fdcb1944
Emiel Post <emielpost@MBP-van-Emiel.home>
2024-05-18 21:10:28 +0200

    Quartz sync: May 18, 2024, 9:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🩸 Hematologie",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
